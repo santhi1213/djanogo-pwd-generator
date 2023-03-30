1b7b9ebdbc9c33ea194b320d7c6ab2598d788b64
Santhi Raju <santhiraju32@gmail.com>
2023-03-30 10:04:22 +0530

    'savepoint1'

---
+++ myapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+
+def password(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    if request.GET.get('symbols'):
+        characters.extend(list('!@#$%&_-+=*^()/'))
+
+    
+
+    thepassword = ''
+
+    length = int(request.GET.get('length'))
+
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+
+    return render (request, 'generator/password.html', {'password':thepassword})
+
+
+
+def about(request):
+    return render(request, 'generator/about.html')
